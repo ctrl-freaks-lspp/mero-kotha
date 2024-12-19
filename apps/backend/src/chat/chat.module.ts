a724fd046be33cf0042403a0b0b2ce3feb120fc6
Aashutosh Pudasaini <nepalidude3@gmail.com>
2024-12-19 22:26:26 +0545

    websockets fix

--- apps/backend/src/chat/chat.module.ts
+++ apps/backend/src/chat/chat.module.ts
@@ -23,7 +23,7 @@ import { MessageService } from './service/message/message.service';
     ]),
     JwtModule.register({
       secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: '1d' },
+      signOptions: { expiresIn:'1w'}
     }),
   ],
   providers: [
